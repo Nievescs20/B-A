dc3f65a68c8fcb08cb722793613c378d799bf77d
ChrisNieves <nievescs20@gmail.com>
2023-03-22 13:17:25 -0400

    hopefully last change to server

--- server/index.cjs
+++ server/index.cjs
@@ -11,6 +11,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
